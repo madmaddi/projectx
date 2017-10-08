dd0d24226a2d4904ee63dc9aaf79f568bd249360
mopmop19@freenet.de <mopmop19@freenet.de>
2017-10-08 23:08:01 +0200

    o

--- window/window/settings.py
+++ window/window/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+# https://stackoverflow.com/a/8675228
+# Find templates in the same folder as settings.py.
+SETTINGS_PATH = os.path.realpath(os.path.dirname(__file__))
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
