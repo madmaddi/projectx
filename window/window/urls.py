dff202828ac32ca1e87bb2ee7d23b5641ea2e5e9
mopmop19@freenet.de <mopmop19@freenet.de>
2017-10-12 23:20:35 +0200

    doc

--- window/window/urls.py
+++ window/window/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^projectx/', include('status.urls')),
+    #url(r'^admin/', admin.site.urls),
+    #url(r'^projectx/', include('status.urls')),
+    url(r'^', include('status.urls')),
 ]
