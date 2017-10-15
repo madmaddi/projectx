14e59453f699cedf0381edcf034308ea3289945a
mopmop19@freenet.de <mopmop19@freenet.de>
2017-10-15 23:01:28 +0200

    up

--- window/window/settings.py
+++ window/window/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=ht7+#@8*71kf3a)o3pdw4e6ic6b&%n4j%+hj+(0nj3%!tiw!g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -124,5 +124,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = "/home/pi/django/projectx/window/static/"
 
 STATIC_URL = '/static/'
+
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
